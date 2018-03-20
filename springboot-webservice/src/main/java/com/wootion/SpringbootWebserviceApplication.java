fbaf98b558aa43845c6e2f8f0b24eb5c065f3796
xzhang <605939936>
2018-03-20 16:49:29 +0800

    example

---
+++ springboot-webservice/src/main/java/com/wootion/SpringbootWebserviceApplication.java
@@ -0,0 +1,22 @@
+package com.wootion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+
+/**
+ * 核心启动类
+ * @author zhangxiaoxiang
+ * @date 2018-03-18
+ */
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class })
+public class SpringbootWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebserviceApplication.class, args);
+	}
+}
